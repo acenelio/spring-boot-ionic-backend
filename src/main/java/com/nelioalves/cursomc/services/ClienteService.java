4842eeb1c9089e3aa0de34561ff7883dc7d879f6
Nelio Alves <acenelio@gmail.com>
2017-09-07 15:19:53 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
