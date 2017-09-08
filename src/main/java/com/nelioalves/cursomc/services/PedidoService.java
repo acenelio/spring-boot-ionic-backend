8c13fb6f1aae44705933a6cca22ca88fcf5c3fa9
Nelio Alves <acenelio@gmail.com>
2017-09-07 22:40:03 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+
+}
